187552597fa476ca733261b619dd3ed93a1847ca
millerladino <millerladino@gmail.com>
2022-02-16 11:44:34 -0500

    guradando cambios de pruebas

---
+++ js/curso.js
@@ -0,0 +1,18 @@
+// console.log('hola Mundo!')
+
+/*asi se representa un comentario de varias lineas en Javascript */
+// Así se representan los comentarios de una sola linea
+
+// var nombre = 'pedro';
+
+// console.log(nombre);
+
+// let nombre = 'alejo';
+
+// console.log(nombre);
+
+const apellido = 'lopez';
+
+console.log('apellido')
+
+apellido = 'gomez';
